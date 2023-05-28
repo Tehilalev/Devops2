e984c5233d2674ec4ff81a65e8b94cdae0d537f2
ShaniSapir <94107440+ShaniSapir@users.noreply.github.com>
2023-05-28 13:49:36 +0300

    opn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const app = require('./server')
+const opn = require('opn');
 
 const port = 3000
 
-app.listen(port, ()=>(
+app.listen(port, ()=>{
     console.log('Server Started!')
-))
+    opn('http://localhost:3000');
+});
