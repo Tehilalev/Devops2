30def7f2decf112d6098802152565eca4e88eca0
tehilalev <tehilalev44@gmail.com>
2024-02-18 19:11:53 +0200

    ver1

--- checkValidation.js
+++ checkValidation.js
@@ -1,25 +1,30 @@
 function checkName(name) {
-    if (/^[A-Za-z]+$/.test(name) && name!= "" && name != null ) {
-      return true;
-    }
-    return false;
+  if (/^[A-Za-z]+$/.test(name) && name != "" && name != null) {
+    return true;
+  }
+  return false;
 }
 
 function checkGrade(grade) {
-    if (!isNaN(grade) && grade >= 0 && grade <= 100 && grade != "" && grade!= null) {
-      return true;
-    }
-    return false;
+  if (
+    !isNaN(grade) &&
+    grade >= 0 &&
+    grade <= 100 &&
+    grade != "" &&
+    grade != null
+  ) {
+    return true;
+  }
+  return false;
 }
 
 try {
-    if (module) {
-      module.exports = {
-        checkName,
-        checkGrade,
-      };
-    }
-}
-catch (e) {
-    console.log(e);
+  if (module) {
+    module.exports = {
+      checkName,
+      checkGrade,
+    };
+  }
+} catch (e) {
+  console.log(e);
 }
